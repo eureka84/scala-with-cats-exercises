07f3959eec397446c51463375691621b529c865f
eureka84 <angelosciarra@ymail.com>
2019-05-20 22:53:40 +0200

    used for comprehension

--- src/test/scala/scala_with_cats/chapter4/MonadTest.scala
+++ src/test/scala/scala_with_cats/chapter4/MonadTest.scala
@@ -8,12 +8,15 @@ class MonadTest extends FunSuite {
   test("Id") {
     import MyMonadInstances._
     import MyMonadSyntax._
-
+˚
     val a: Id[Int] = 4.pure[Id]
     val b: Id[Int] = 5.pure[Id]
 
     def square[F[_]](x: F[Int], y: F[Int])(implicit m: MyMonad[F]): F[Int] =
-      m.flatMap(x)(a => m.map(y)(b => a * a + b * b))
+      for {
+        a <- x
+        b <- y
+      } yield a * a + b * b
 
     assertResult(square(a, b))(41)
   }
