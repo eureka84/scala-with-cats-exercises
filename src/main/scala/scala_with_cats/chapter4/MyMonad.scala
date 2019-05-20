43d44136be5ce6475cf263ee5df707a977984f76
eureka84 <angelosciarra@ymail.com>
2019-05-20 22:11:35 +0200

    added monad syntax

--- src/main/scala/scala_with_cats/chapter4/MyMonad.scala
+++ src/main/scala/scala_with_cats/chapter4/MyMonad.scala
@@ -5,7 +5,6 @@ import scala_with_cats.chapter4.Id.Id
 import scala.language.higherKinds
 
 trait MyMonad[F[_]] {
-
   def pure[A](a: A): F[A]
 
   def flatMap[A, B](value: F[A])(func: A => F[B]): F[B]
@@ -19,14 +18,24 @@ object Id {
 }
 
 
-object IdInstances {
+object MyMonadInstances {
 
   implicit val myIdMonad: MyMonad[Id] = new MyMonad[Id] {
-
     override def pure[A](a: A): Id[A] = a
 
     override def flatMap[A, B](value: Id[A])(func: A => Id[B]): Id[B] = func(value)
+  }
+}
 
+object MyMonadSyntax {
+
+  implicit class MonadOps[F[_], A](a: F[A]) {
+    def flatMap[B](func: A => F[B])(implicit m: MyMonad[F]): F[B] = m.flatMap(a)(func)
+
+    def map[B](func: A => B)(implicit m: MyMonad[F]): F[B] = m.map(a)(func)
   }
 
+  implicit class ApplicativeOps[A](a: A) {
+    def pure[F[_]](implicit m: MyMonad[F]): F[A] = m.pure(a)
+  }
 }
--- src/test/scala/scala_with_cats/chapter4/MonadTest.scala
+++ src/test/scala/scala_with_cats/chapter4/MonadTest.scala
@@ -6,14 +6,14 @@ import scala_with_cats.chapter4.Id.Id
 class MonadTest extends FunSuite {
 
   test("Id") {
-    import IdInstances._
+    import MyMonadInstances._
+    import MyMonadSyntax._
 
-    val a: Id[Int] = 4
-    val b: Id[Int] = 5
+    val a: Id[Int] = 4.pure[Id]
+    val b: Id[Int] = 5.pure[Id]
 
     def square[F[_]](x: F[Int], y: F[Int])(implicit m: MyMonad[F]): F[Int] =
       m.flatMap(x)(a => m.map(y)(b => a * a + b * b))
-
 
     assertResult(square(a, b))(41: Id[Int])
   }
