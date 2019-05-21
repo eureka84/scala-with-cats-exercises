cb940c8a61fbd074b3d2e93cfeac387ddac9d1b6
eureka84 <angelosciarra@ymail.com>
2019-05-21 10:29:05 +0200

    fixed imports

--- src/test/scala/scala_with_cats/chapter4/FutureTests.scala
+++ src/test/scala/scala_with_cats/chapter4/FutureTests.scala
@@ -3,6 +3,8 @@ package scala_with_cats.chapter4
 import cats.Monad
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
+import cats.instances.future._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object FutureTests extends App {
   val fm: Monad[Future] = Monad[Future]
